0f0f7cbee754686850bf52dd02ca35cc9911c348
albertmarsnik <albertmarsnik@gmail.com>
2020-08-19 17:16:02 +0200

    task/module-3-creating-and-communicating-between-angular-components _ added event-thumbnail component.

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { EventAppComponent } from './app.component';
 import { EventsListComponent } from './events/list/events-list.component';
+import { EventsThumbnailComponent } from './events/thumbnail/events-thumbnail.component';
 
 @NgModule({
-  declarations: [EventAppComponent, EventsListComponent],
+  declarations: [EventAppComponent, EventsListComponent, EventsThumbnailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [EventAppComponent],
