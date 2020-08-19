e33c249de71c08abf614fc6b1d02281bb293c844
albertmarsnik <albertmarsnik@gmail.com>
2020-08-19 17:10:17 +0200

    task/module-2-getting-started-with-angular _ basic display of an event list setup with 1 hard coded event.

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventAppComponent } from './app.component';
+import { EventsListComponent } from './events/list/events-list.component';
 
 @NgModule({
-  declarations: [EventAppComponent],
+  declarations: [EventAppComponent, EventsListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [EventAppComponent],
